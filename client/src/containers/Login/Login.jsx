f61b0eb054b815a9d9171409a437a094928d2c6e
Vincent Kendrick <dagreatcode@gmail.com>
2023-01-10 09:57:39 -0500

    added the nav links

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -3,24 +3,24 @@ import React from "react";
 export default function Login() {
   return (
     <>
-      <div class="mb-3">
-        <label htmlFor="exampleFormControlInput1" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
           Email address
         </label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="name@example.com"
         />
       </div>
-      <div class="mb-3">
-        <label htmlFor="exampleFormControlInput2" class="form-label">
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput2" className="form-label">
           Password
         </label>
         <input
           type="password"
-          class="form-control"
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="Password Here"
         />
