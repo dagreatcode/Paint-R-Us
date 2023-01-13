e0ae89200a75b19549be56aeb8b27aecb4dbb334
Vincent Kendrick <dagreatcode@gmail.com>
2023-01-13 17:59:55 -0500

    added images and updated them to home page

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -12,19 +12,31 @@ const Home = () => {
     <>
       <Banner />
       <div className="container">
-        <div className="row">
-          <div className="card text-bg-dark">
-            <img src={ServiceMan} className="card-img" alt="..." />
-            <div className="card-img-overlay">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small>Last updated 3 mins ago</small>
-              </p>
+        <div className="row" style={{display: "flex", justifyContent: "center", textAlign: "center"}}>
+          <div className="card mb-3" style={{ maxWidth: "840px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img
+                  src={ServiceMan}
+                  className="img-fluid rounded-start"
+                  alt="..."
+                />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
