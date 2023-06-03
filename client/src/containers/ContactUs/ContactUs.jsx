d30483434dd2ea4ddef388f6509938e194bebf7e
Vincent Kendrick <dagreatcode@gmail.com>
2023-06-03 02:26:09 -0400

    updating the views

--- client/src/containers/ContactUs/ContactUs.jsx
+++ client/src/containers/ContactUs/ContactUs.jsx
@@ -171,13 +171,49 @@ const ContactUs = () => {
               </div>
             </div>
           </div>
-          <Button onClick={handleSubmit} style={{
-              display: "flex",
-              justifyContent: "center",
-              textAlign: "center",
-            }}>go</Button>
+          <div></div>
+          <div className="row">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </div>
+        <Button
+          onClick={handleSubmit}
+          style={{
+            margin: "auto",
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          Send Request
+        </Button>
       </form>
+      <iframe
+        title="myFrame"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
+        // frameBorder="0"
+        width="600"
+        height="450"
+        style={{
+          padding: "10px",
+          border: "0",
+          margin: "auto",
+          allowFullScreen: "",
+          ariaHidden: "false",
+          tabIndex: "0",
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
+      />
     </>
   );
 };
