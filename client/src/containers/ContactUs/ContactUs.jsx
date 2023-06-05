c0ef410b8f99a122734ed574602450f0601e34c3
Vincent Kendrick <dagreatcode@gmail.com>
2023-06-04 22:28:08 -0400

    next, cleanup

--- client/src/containers/ContactUs/ContactUs.jsx
+++ client/src/containers/ContactUs/ContactUs.jsx
@@ -141,9 +141,9 @@ const ContactUs = () => {
             </p>
           </div>
           <br />
-        <br />
-        <br />
-        <br />
+          <br />
+          <br />
+          <br />
           <div
             className="row"
             style={{
@@ -237,22 +237,22 @@ const ContactUs = () => {
             </div>
           </div>
           <Button
-          type="submit"
-          onClick={handleSubmit}
-          style={{
-            margin: "auto",
-            display: "flex",
-            justifyContent: "center",
-            textAlign: "center",
-          }}
-        >
-          Send Request
-        </Button>
-        <br />
-        <br />
-        <br />
-        <br />
-          <div style={{margin: "50px"}}margin="200px" className="row">
+            type="submit"
+            onClick={handleSubmit}
+            style={{
+              margin: "auto",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            Send Request
+          </Button>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div style={{ margin: "20px" }} className="row">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -264,9 +264,7 @@ const ContactUs = () => {
             </p>
           </div>
         </div>
-      
       </form>
-  
       <iframe
         title="myFrame"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
@@ -285,14 +283,14 @@ const ContactUs = () => {
           textAlign: "center",
         }}
       />
-           <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 };
